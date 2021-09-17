6f12ffffcf92c82cbc5a12dbdafc0f9e619d904f
Mohamed Shelf <mohamedsaidlearn@gmail.com>
2021-09-17 17:07:01 +0200

    1. changed active sidebar icon color when active 2. added bottom margin to new Tweet

--- src/Components/SideBar/SideBarOption.js
+++ src/Components/SideBar/SideBarOption.js
@@ -7,8 +7,8 @@ function SideBarOption({ Icon, ActiveIcon, text, coloredIcon }) {
   useEffect(() => {});
   return (
     <div class={`side-bar-option ${coloredIcon ? 'coloredIcon' : ''}`}>
-        <Icon class="side-bar__icon" />
-        <span className="side-bar__txt">{text}</span>
+      <Icon class="side-bar__icon" />
+      <span className="side-bar__txt">{text}</span>
     </div>
   );
 }
