c7278399b2d257c886b6ef9ebf175c69046c4237
Mohamed Shelf <mohamedsaidlearn@gmail.com>
2021-09-27 21:24:34 +0200

    changed files extentions from .js to .jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,18 +2,18 @@ import SvgSparkle from '../../media/icons/components/Sparkle';
 import NewTweet from './NewTweet';
 import Tweet from './Tweet';
 import Widgets from './Widgets';
-import db from '../../firebase/firebase';
+import db from '../../firebase';
 import { useEffect, useState } from 'react';
 
 function Home() {
   // firebase stuff
-  const [tweets, setTweets] = useState([]);
+  const [tweets, setTweets] = useState([{}]);
 
-  useEffect(() => {
-    db.collection('posts').onSnapshot((snapshot) =>
-      setTweets(snapshot.docs.map((doc) => doc.data()))
-    );
-  }, []);
+  // useEffect(() => {
+  //   db.collection('posts').onSnapshot((snapshot) =>
+  //     setTweets(snapshot.docs.map((doc) => doc.data()))
+  //   );
+  // }, []);
 
   const scrollToTop = (e) => {
     e.preventDefault();
@@ -40,7 +40,7 @@ function Home() {
         <NewTweet />
         {/* Tweets */}
         <div className="tweets-wrapper">
-          {tweets.map((post) => (
+          {/* {tweets.map((post) => (
             <Tweet
               key={post.text}
               displayName={post.displayName}
@@ -51,8 +51,8 @@ function Home() {
               image={post.image}
               timestamp={1518122597860}
             />
-          ))}
-          {/* <Tweet
+          ))} */}
+          <Tweet
             displayName="Mohamed Shelf"
             userID="mohamed"
             text="Hi there this is my first post"
@@ -75,7 +75,31 @@ function Home() {
             image="https://images.pexels.com/photos/4916147/pexels-photo-4916147.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
             avatar="https://images.pexels.com/photos/1080213/pexels-photo-1080213.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
             timestamp={1517043995650}
-          /> */}
+          />
+          <Tweet
+            displayName="Ahmed Moaaz"
+            userID="ahmed_moaz"
+            text="Hi there this is my first post"
+            image="https://images.unsplash.com/photo-1632715413137-17b569a15318?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+            avatar="https://images.unsplash.com/photo-1624298357597-fd92dfbec01d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGF2YXRhcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+            timestamp={1517043995650}
+          />
+          <Tweet
+            displayName="Mohamed Mohsen"
+            userID="mohhhhsen"
+            text="Hi there this is my first post"
+            image="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+            avatar="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+            timestamp={1517043995650}
+          />
+          <Tweet
+            displayName="Mohamed Hossain"
+            userID="hosain_mo"
+            text="Hi there this is my first post"
+            image="https://images.unsplash.com/photo-1632714393270-df9f55978898?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+            avatar="https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+            timestamp={1517043995650}
+          />
         </div>
       </div>
       {/* newest */}
---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// // import firebase from 'firebase';
+// const firebase = require('firebase');
+
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyDYWeLAfcDQEaWyQh3Poyy0BxRNUz9dgIw',
+
+//   authDomain: 'twitter-d697c.firebaseapp.com',
+
+//   projectId: 'twitter-d697c',
+
+//   storageBucket: 'twitter-d697c.appspot.com',
+
+//   messagingSenderId: '253707320020',
+
+//   appId: '1:253707320020:web:173a36abd3183f90969941',
+
+//   measurementId: 'G-56MLG2KGVR',
+// };
+
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// const db = firebaseApp.firestore();
+
+// export default db;
